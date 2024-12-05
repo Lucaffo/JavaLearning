9dc399f62a6d731966a3f540201194ee8ec4b5f1
Luca Raffo <55745404+Lucaffo@users.noreply.github.com>
2024-12-05 23:32:48 +0100

    Project Initialization

---
+++ dinotodo/src/main/java/com/lucaffo/dinotodo/DinotodoApplication.java
@@ -0,0 +1,13 @@
+package com.lucaffo.dinotodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinotodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinotodoApplication.class, args);
+	}
+
+}
---
+++ dinotodo/src/test/java/com/lucaffo/dinotodo/DinotodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaffo.dinotodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinotodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
