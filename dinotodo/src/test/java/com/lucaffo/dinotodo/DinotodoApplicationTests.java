e68af87a19d6956adcfb753ba077d05ad6a9e0f8
Luca Raffo <55745404+Lucaffo@users.noreply.github.com>
2024-12-06 03:01:31 +0100

    Removed stupid test that need to log to db to 'mvn clean package'

--- dinotodo/src/test/java/com/lucaffo/dinotodo/DinotodoApplicationTests.java
+++ dinotodo/src/test/java/com/lucaffo/dinotodo/DinotodoApplicationTests.java
@@ -3,10 +3,8 @@ package com.lucaffo.dinotodo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DinotodoApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
